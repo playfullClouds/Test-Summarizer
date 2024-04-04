4878d1d57aec930ec53144a81fea2550e7960b38
playfullClouds <161916509+playfullClouds@users.noreply.github.com>
2024-04-04 17:37:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,67 @@
 from pathlib import Path
+import os
 
 
 from textSummarizer.logging import logger
-# from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-# from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-# from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 
-CONFIG_FILE_PATH = Path('C:/Test-Summarizer/config/config.yaml')  
-PARAMS_FILE_PATH = Path('C:/Test-Summarizer/params.yaml')
+# CONFIG_FILE_PATH = Path('C:/Test-Summarizer/config/config.yaml')  
+# PARAMS_FILE_PATH = Path('C:/Test-Summarizer/params.yaml')
 
 
 
-# STAGE_NAME = "Data Ingestion stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataIngestionTrainingPipeline(CONFIG_FILE_PATH, PARAMS_FILE_PATH)
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
-    
-    
-# STAGE_NAME = "Data Validation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_validation = DataValidationTrainingPipeline(CONFIG_FILE_PATH, PARAMS_FILE_PATH)
-#    data_validation.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
-    
-    
-# STAGE_NAME = "Data Transformation stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_transformation = DataTransformationTrainingPipeline(CONFIG_FILE_PATH, PARAMS_FILE_PATH)
-#    data_transformation.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
-    
-    
-# STAGE_NAME = "Model Training stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_trainer = ModelTrainerTrainingPipeline(CONFIG_FILE_PATH, PARAMS_FILE_PATH)
-#    data_trainer.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
-     
+CONFIG_FILE_PATH = Path(os.getenv('CONFIG_FILE_PATH', './config/config.yaml'))
+PARAMS_FILE_PATH = Path(os.getenv('PARAMS_FILE_PATH', './params.yaml'))
 
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline(CONFIG_FILE_PATH, PARAMS_FILE_PATH)
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline(CONFIG_FILE_PATH, PARAMS_FILE_PATH)
+   data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline(CONFIG_FILE_PATH, PARAMS_FILE_PATH)
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+STAGE_NAME = "Model Training stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_trainer = ModelTrainerTrainingPipeline(CONFIG_FILE_PATH, PARAMS_FILE_PATH)
+   data_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 STAGE_NAME = "Model Evaluation stage"
